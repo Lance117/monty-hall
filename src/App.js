f40b7ac88024cda5af2233482fcb5d2d5027b1d5
Lance Wong <lwong180@gmail.com>
2019-11-26 12:03:51 -0800

    begin layout of app, hardcoding chart values for now

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import Keyboard, { Cursor } from 'react-mk'
+import Select from 'react-select';
+import SimBarGraph from './SimBarGraph'
+import './App.css';
+
+class Door extends React.Component {
+  render() {
+    return (
+      <div className="door">
+        <img src="https://www.pnglot.com/pngfile/detail/172-1721328_tardis-door-opening-gif.png" alt="tardis-door" onClick={() => console.log("I'm a door!")}></img>
+      </div>
+    );
+  }
+}
+
+class MontyGame extends React.Component {
+  renderDoor(i) {
+    return <Door />;
+  }
+
+  render() {
+    const status = 'Choose your door: ';
+
+    return (
+      <div className="monty-game">
+        <div className="doors">
+          {this.renderDoor(0)}
+          {this.renderDoor(1)}
+          {this.renderDoor(2)}
+        </div>
+        <div className="status">
+          <Keyboard sentenceDelayPerCharRange={[0, 0]}>{status}</Keyboard>
+          <Cursor />
+        </div>
+      </div>
+    )
+  }
+}
+
+class MontySim extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numTrials: null
+    };
+
+  }
+
+  handleChange = selectedOption => {
+    this.setState({numTrials: selectedOption.value});
+  }
+
+  render() {
+    const { selectedOption } = this.state;
+    return (
+      <div className="monty-sim">
+        <div style={{width: "300px"}}>
+          <Select
+            value={selectedOption}
+            onChange={this.handleChange}
+            options={options}
+            placeholder="Welcome to simulator! Please choose N rounds:"
+          />
+        </div>
+        <SimBarGraph numTrials={this.state.numTrials}/>
+      </div>
+    )
+  }
+}
+
+// Variables
+const options = [
+  {value: 50, label: '50'},
+  {value: 100, label: '100'},
+  {value: 500, label: '500'},
+  {value: 1000, label: '1000'},
+];
+//---------
+
+const App = () => (
+  <div className="app">
+    <MontyGame />
+    <MontySim />
+    <footer className="main-footer">Hello world</footer>
+  </div>
+)
+
+export default App;
---
+++ src/SimBarGraph.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import {HorizontalBar} from 'react-chartjs-2';
+
+export default class SimBarGraph extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numTrials: this.props.numTrials
+        }
+
+        this.chartReference = {};
+    }
+
+    componentDidUpdate() {
+        if (this.props.numTrials) {
+            // for (let i = 1; i <= this.props.numTrials; i++) {
+
+            // }
+        }
+    }
+
+    componentDidMount() {
+        console.log(this.chartReference)
+    }
+
+    // helper variables & functions
+    data = {
+        labels: ['Stay', 'Switch'],
+        datasets: [
+            {
+                label: 'Win %',
+                backgroundColor: 'rgba(122,200,0,0.6)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: [50, 50]
+            },
+            {
+                label: 'Loss %',
+                backgroundColor: 'rgba(242,97,48,0.6)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: [50, 50]
+            }
+        ]
+    };
+
+    updateData = (chart, stayWinRate) => {
+        chart.props.data.datasets[0].data = [stayWinRate, 100 - stayWinRate]; 
+        chart.chartInstance.update();
+    }
+    //-----------------
+
+    render() {
+        return (
+            <div style={{width: "500px"}}>
+                <h2>Win Rate by Strategy</h2>
+                <HorizontalBar
+                    ref={(reference) => this.chartReference = reference}
+                    data={this.data}
+                    options={{
+                        responsive: true,
+                        scales: {
+                            yAxes: [{
+                                stacked: true,
+                                barThickness: "flex",
+                                fontSize: 15,
+                                categoryPercentage: 0.5
+                            }],
+                            xAxes: [{
+                                stacked: true,
+                                ticks: {
+                                    beginAtZero: true,
+                                    fontSize: 15
+                                }
+                            }]
+                        },
+                        legend: {display: false},
+                    }}
+                />
+            </div>
+        )
+    }
+}
