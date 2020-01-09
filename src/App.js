034e47879b864897e50c6f172d9ef8b62f9822b5
Lance Wong <lwong180@gmail.com>
2020-01-09 14:04:29 -0800

    add popup for basic info about this project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MontySim from './MontySim'
 import MontyGame from './MontyGame'
 import './App.css';
 
-const App = () => (
-  <div className="app">
-    <MontyGame />
-    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-      <MontySim />
-      <img src="https://upload.wikimedia.org/wikipedia/en/b/be/Probability_diagram_for_Monty_Hall_problem.gif" alt="prob-diagram"></img>
+const App = () => {
+  const [showSlidein, setSlidein] = useState(true);
+
+  return (
+    <div className="app">
+      <MontyGame />
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+        <MontySim />
+        <img src="https://upload.wikimedia.org/wikipedia/en/b/be/Probability_diagram_for_Monty_Hall_problem.gif" alt="prob-diagram"></img>
+      </div>
+      {showSlidein && <aside className="slidein">
+        <section className="slidein-content">
+          <p>
+            Hello! This app is about the <a href="https://en.wikipedia.org/wiki/Monty_Hall_problem">Monty Hall problem.</a> 
+            There's a prize behind one of the three doors. How to play:
+          </p>
+          <ol>
+            <li>Choose a door.</li>
+            <li>Switch doors or keep the same one.</li>
+            <li>You've won or lost.</li>
+          </ol>
+          There's also a simulator to help prove that switching doors is the best strategy. Try it out!
+          <button className="header-btn" onClick={() => setSlidein(false)}>Got It!</button>
+        </section>
+      </aside>}
     </div>
-  </div>
-)
+  )
+}
 
 export default App;
