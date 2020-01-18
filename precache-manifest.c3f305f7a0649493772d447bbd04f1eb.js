4a37c90dc34501f4f068827f3c5aba1a668138de
Lance Wong <lwong180@gmail.com>
2020-01-17 23:19:56 -0800

    Updates

--- precache-manifest.c3f305f7a0649493772d447bbd04f1eb.js
+++ precache-manifest.c3f305f7a0649493772d447bbd04f1eb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c003b664725173a15eba3dee81bb73bc",
+    "revision": "53810d032741d7a0b6b2af5e040c86aa",
     "url": "/monty-hall/index.html"
   },
   {
-    "revision": "40e80a4915d1dc94bf9e",
+    "revision": "ea05dbf8f3dd745cc2a3",
     "url": "/monty-hall/static/css/main.8bd8f2a6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/monty-hall/static/js/2.3b089ede.chunk.js"
   },
   {
-    "revision": "40e80a4915d1dc94bf9e",
-    "url": "/monty-hall/static/js/main.790b97cc.chunk.js"
+    "revision": "ea05dbf8f3dd745cc2a3",
+    "url": "/monty-hall/static/js/main.0f5bbeda.chunk.js"
   },
   {
     "revision": "c3b36accd637a3871220",
